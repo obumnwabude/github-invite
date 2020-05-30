1d4744479066561636eae4fa9f0a179c4ba23be1
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-05-30 13:59:07 +0100

    start static-frontend version of invite-app

---
+++ node-monolith/README.md
@@ -0,0 +1,20 @@
+# GitHub Organisation Invitation
+
+A web app that can accept a GitHub username and send them an invitation to join a GitHub Organisation.
+
+## Add org.js file
+Add an org.js file in the root of this repository for this app to work. The file should export the name of the GitHUB organisation to be joined and an OAuth access token.
+
+Only admin members of a GitHub organisation can send an invitation to other GitHub users, to join that organisation.
+The admin member must be authenticated before invitation can be sent. Go to settings in admin's GitHub account and get a new OAuth access token or use an existing token, if you already have one. Create a file in the root of this repository and name it `org.js`.
+Export an object containing the name of the organisation to joined and your OAuth token.
+
+Example: in `org.js` 
+```
+module.exports = {
+  name: <NAME OF ORGANISATION ON GITHUB>,
+  token: <OAUTH ACCESS TOKEN OF AN ADMIN IN THAT ORGANISATION>
+};
+```
+
+`org.js` has been added to .gitignore so that credentials are not committed to Git.
---
+++ node-monolith/public/app.js
@@ -0,0 +1,81 @@
+// get the DOM objects
+const inviteForm = document.querySelector('#invite');
+const inputUsername = document.querySelector("input[type='text']");
+const submitButton = document.querySelector("button[type='submit']");
+const messageContainer = document.querySelector('#message-container');
+const messageHeading = document.querySelector('#message-heading');
+const messageBody = document.querySelector('#message-body');
+const org = document.querySelector('.org');
+
+// get organisation's name 
+fetch('/org')
+  .then(response => response.text())
+  .then(name => {
+    if (name !== '') {
+      console.log(name);
+      org.innerHTML = '@' + name;
+      org.classList.add('org-name');
+    }
+  }).catch(error => console.log(error));
+
+// ensures that the messageContainer when a username is being entered
+inputUsername.addEventListener('focus', () => {
+  if (!(messageContainer.classList.contains('hide'))) messageContainer.classList.add('hide');
+  if (submitButton.disabled) submitButton.disabled = false;
+});
+
+// capture submit events and send
+inviteForm.addEventListener('submit', e => {
+  e.preventDefault();
+  // make the click button unavailable
+  submitButton.disabled = true;
+  // send username and handle accordingly
+  fetch('/', { 
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept':'application/json'
+    },
+    body: JSON.stringify(Object.fromEntries(new FormData(inviteForm)))})
+    .then(response => response.json())
+    .then(data => displayMessage(data))
+    .catch(error => displayMessage({
+      status: false,
+      message: 'An Error Occured',
+      body: error
+    }));
+});
+
+/** 
+ * Adds the response message to the DOM
+ * @param {Object} data - the data from which the message to be added to the DOM is configured
+ */
+const displayMessage = (data) => {
+  // if invitation was successfully sent
+  if (data.status) {
+    // set the color of header
+    messageHeading.classList.remove('failure');
+    messageHeading.classList.add('success');
+    // update the DOM
+    addToDom(data);
+  } else {
+  // else if invitation was not successfully sent
+    // set the color of header
+    messageHeading.classList.remove('success');
+    messageHeading.classList.add('failure');
+    // update the DOM
+    addToDom(data);
+  }
+  // make the click button available
+  submitButton.disabled = false;
+};
+
+/** 
+ * Adds the provided message and body to the DOM and expose them
+ * @param {Object} data - the data to be added to the DOM
+ */
+const addToDom = data => {
+  messageHeading.innerHTML = data.message;
+  messageBody.innerHTML = data.body;
+  messageContainer.classList.remove('hide');
+};
