68c50c63c8469b174c258dd2155550ba85dee3e3
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-06-17 16:38:35 +0100

    update help to reflect multi-argument changes

--- npm-package/lib/cmds/help.js
+++ npm-package/lib/cmds/help.js
@@ -5,8 +5,12 @@ const cyan = (text) => chalk.cyan.bold(text);
 module.exports = () => {
   const help = `Invites a GitHub user with the provided username to the provided GitHub organization. The personal access token of an admin in that GitHub organization is required and it is with it that the invitation is done.
 
-Usage: github-invite <org> <username> <token>
+Usage: github-invite
+       github-invite <org>
+       github-invite <org> <username> <token>
        github-invite [options]
+
+Use without arguments or with incomplete arguments for an interactive invitation. Using without arguments is preferrable because token input is hidden.
 
   ${cyan('org')}          The name of an organization in GitHub into which a user
                is to be invited.
