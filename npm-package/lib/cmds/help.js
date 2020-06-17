d9a1b0d0f46950112865e0a86c5ca9fc566b3e69
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-06-17 10:59:41 +0100

    update help to use cyan instead of blue

--- npm-package/lib/cmds/help.js
+++ npm-package/lib/cmds/help.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 
-const blue = (text) => chalk.blueBright.bold(text);
+const cyan = (text) => chalk.cyan.bold(text);
 
 module.exports = () => {
   const help = `Invites a GitHub user with the provided username to the provided GitHub organization. The personal access token of an admin in that GitHub organization is required and it is with it that the invitation is done.
@@ -8,18 +8,18 @@ module.exports = () => {
 Usage: github-invite <org> <username> <token>
        github-invite [options]
 
-  ${blue('org')}          The name of an organization in GitHub into which a user
+  ${cyan('org')}          The name of an organization in GitHub into which a user
                is to be invited.
-  ${blue('username')}     The username of the GitHub user to be invited to the provided
+  ${cyan('username')}     The username of the GitHub user to be invited to the provided
                organization.
-  ${blue('token')}        A personal access token of an admin member in the organization.
+  ${cyan('token')}        A personal access token of an admin member in the organization.
                The token must at least have the write:org permission under
                admin:org permissions when the token was created in GitHub 
                account settings of the admin.
 
 Options:
-  help, --help, or -h:          Displays this help information.
-  version, --version, or -v:    Displays the current version of github-invite.
+  ${cyan('help')}, ${cyan('--help')}, or ${cyan('-h')}:          Displays this help information.
+  ${cyan('version')}, ${cyan('--version')}, or ${cyan('-v')}:    Displays the current version of github-invite.
   `;
 
   console.log(help);
