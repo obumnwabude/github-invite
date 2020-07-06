a744eef81aac5e7944a73a743009f755d88a5e80
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-07-06 16:53:06 +0100

    add lines to help to ease readability

--- npm-package/lib/cmds/help.js
+++ npm-package/lib/cmds/help.js
@@ -3,12 +3,13 @@ const chalk = require('chalk');
 const cyan = (text) => chalk.cyan.bold(text);
 
 module.exports = () => {
-  const help = `Invites a GitHub user with the provided username to the provided GitHub organization. The personal access token of an admin in that GitHub organization is required and it is with it that the invitation is done.
+  const help = `
+Invites a GitHub user with the provided username to the provided GitHub organization. The personal access token of an admin in that GitHub organization is required and it is with it that the invitation is done.
 
 Usage: github-invite
        github-invite <org>
        github-invite <org> <username> <token>
-       github-invite [options]
+       github-invite <options>
 
 Use without arguments or with incomplete arguments for an interactive invitation. Using without arguments is preferrable because token input is hidden.
 
@@ -16,10 +17,12 @@ Use without arguments or with incomplete arguments for an interactive invitation
     'org'
   )}          The name of an organization in GitHub into which a user
                is to be invited.
+
   ${cyan(
     'username'
   )}     The username of the GitHub user to be invited to the provided
                organization.
+
   ${cyan(
     'token'
   )}        A personal access token of an admin member in the organization.
@@ -28,9 +31,11 @@ Use without arguments or with incomplete arguments for an interactive invitation
                account settings of the admin.
 
 Options:
+
   ${cyan('help')}, ${cyan('--help')}, or ${cyan(
     '-h'
   )}:          Displays this help information.
+
   ${cyan('version')}, ${cyan('--version')}, or ${cyan(
     '-v'
   )}:    Displays the current version of github-invite.
